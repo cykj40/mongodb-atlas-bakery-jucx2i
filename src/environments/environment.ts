4b27f77465fa37905b6cefda37878de1224b9052
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-01-15 18:52:47 -0500

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  APP_ID: '',
-  GRAPHQL_URI: '',
-  API_KEY: '',
-};
+  production: true,
+  APP_ID: 'bakery-jmldl',
+  GRAPHQL_URI: 'https://us-east4.gcp.realm.mongodb.com/api/client/v2.0/app/bakery-jmldl/graphql',
+  API_KEY: 'O46hOmZgWf7gBsdPPq0w1G058hvLn5xZKfGlHNHpoEWM4CbjZ8ZQtenslURjDWBg'
+  };
